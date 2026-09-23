273a9efec1057f7b65f167ac171fbfef93d54d21
felipe01-nav <felipe01@nav.com>
2026-09-23 09:55:25 -0600

    v4.73: bump CACHE_VERSION

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Bump CACHE_VERSION whenever a release should discard old cached assets,
    and bump ?v= in index.html at the same time. */
 
-const CACHE_VERSION = 'nb-2026-09-23-g';
+const CACHE_VERSION = 'nb-2026-09-23-h';
 const ART_ASSETS = ['art-core.js', 'art-merge.js', 'art-v47.js'];
 
 self.addEventListener('install', (event) => {
